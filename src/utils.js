b225323094f9afc91bd96e2abe49949266338ce2
Ko-Gyeongtae <kokt0203@naver.com>
2021-01-11 20:17:36 +0900

    fix Error & reinstall modules

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,34 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import mgTransport from "nodemailer-mailgun-transport";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+const sendMail = (email) => {
+  const options = {
+    auth: {
+      api_key: process.env.MAILGUN_API,
+      domain: process.env.MAILGUN_DOMAIN,
+    },
+  };
+  const client = nodemailer.createTransport(mgTransport(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (adress, secret) => {
+  const email = {
+    from: "instagram@prismagram.com",
+    to: adress,
+    subject: "🔒Login Secret for Prismagram🔒",
+    html: `Hello! Your login secret it ${secret}.
+    <br/>Copy paste on the app/website to log in`,
+  };
+  return sendMail(email);
 };

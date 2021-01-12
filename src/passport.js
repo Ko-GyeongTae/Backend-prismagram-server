716b08fdc9a53e03361b060773b84f272abc660e
Ko-Gyeongtae <kokt0203@naver.com>
2021-01-12 11:34:18 +0900

    Generate Token API

--- src/passport.js
+++ src/passport.js
@@ -10,8 +10,15 @@ const jwtOptions = {
     secret: process.env.JWT_SECRET
 };
 
-const verifyUser = (payload, done) => {
-    console.log(payload);
+const verifyUser = async(payload, done) => {
+    try{
+        const user = await prisma.user({id: payload.id});
+        if(user !== null){
+            return done(null, user);
+        } else {
+            return done(null, false);
+        }
+    }
 }
 
 passport.use(new JwtStrategy(jwtOptions, verifyUser));

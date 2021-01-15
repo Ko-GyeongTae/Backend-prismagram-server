cc9f3515e795ab62864fec9ccb6181e994b5c21d
Ko-Gyeongtae <kokt0203@naver.com>
2021-01-15 11:49:02 +0900

    check request

--- src/passport.js
+++ src/passport.js
@@ -1,9 +1,6 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, ".env")});
-
 import passport from "passport";
 import {Strategy, ExtractJwt} from "passport-jwt";
+import { prisma } from "../generated/prisma-client";
 
 const jwtOptions = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),

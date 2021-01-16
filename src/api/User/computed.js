ddde78a85b1278bbe1c09a75cf1d467c77ff9a7f
Ko-GyeongTae <kokt0203@naver.com>
2021-01-16 22:27:55 +0900

    Edit: create isLiked in Post

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -10,7 +10,7 @@ export default {
       const { id: parentId } = parent;
       try {
         return prisma.$exists.user({
-          AND: [{ id: parentId }, { followers_some: { id: user.id }}],
+          AND: [{ id: parentId }, { followers_some: { id: user.id } }],
         });
       } catch (error) {
         return false;
@@ -20,6 +20,26 @@ export default {
       const { user } = request;
       const { id: parentId } = parent;
       return user.id === parentId;
-    },
+    }
   },
+  Post: {
+    isLiked: (parent, _, { request }) => {
+      const { user } = request;
+      const { id } = parent;
+      return prisma.$exists.like({
+        AND: [
+          {
+            user: {
+              id: user.id,
+            },
+          },
+          {
+            post: {
+              id
+            }
+          }
+        ]
+      });
+    }
+  }
 };

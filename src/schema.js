06301dfb747d6ce8d6572754ff86d1426a49147a
Ko-GyeongTae <kokt0203@naver.com>
2021-01-21 00:03:40 +0900

    Edit: schema.js

--- src/schema.js
+++ src/schema.js
@@ -1,14 +1,13 @@
 import path from "path";
-import { makeExecutableSchema } from "@graphql-tools/schema";
-import { mergeTypeDefs, mergeResolvers  } from "@graphql-tools/merge";
-import { loadFilesSync } from "@graphql-tools/load-files";
+import { makeExecutableSchema } from "graphql-tools";
+import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
 
-const allTypes = loadFilesSync(path.join(__dirname, "/api/**/*.graphql"));
-const allResolvers = loadFilesSync(path.join(__dirname, "/api/**/*.js"));
+const allTypes = fileLoader(path.join(__dirname, "/api/**/*.graphql"));
+const allResolvers = fileLoader(path.join(__dirname, "/api/**/*.js"));
 
 const schema = makeExecutableSchema({
-    typeDefs: mergeTypeDefs(allTypes),
-    resolvers: mergeResolvers(allResolvers)
+  typeDefs: mergeTypes(allTypes),
+  resolvers: mergeResolvers(allResolvers)
 });
 
 export default schema;

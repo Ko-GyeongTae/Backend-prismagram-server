08151688a4164e807dfedd76ad2723f22d4553aa
Ko-GyeongTae <kokt0203@naver.com>
2021-01-21 00:53:40 +0900

    Edit: Backend

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 import { adjectives, nouns } from "./words";
 import nodemailer from "nodemailer";
-import sgTransport from "nodemailer-sendgrid-transport";
+import mgTransport from "nodemailer-mailgun-transport";
 import jwt from "jsonwebtoken";
 
 export const generateSecret = () => {
@@ -8,20 +8,20 @@ export const generateSecret = () => {
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
 };
 
-const sendMail = email => {
+const sendMail = (email) => {
   const options = {
     auth: {
-      api_user: process.env.SENDGRID_USERNAME,
-      api_key: process.env.SENGRID_PASSWORD
-    }
+      api_key: process.env.MAILGUN_API,
+      domain: process.env.MAILGUN_DOMAIN,
+    },
   };
-  const client = nodemailer.createTransport(sgTransport(options));
+  const client = nodemailer.createTransport(mgTransport(options));
   return client.sendMail(email);
 };
 
 export const sendSecretMail = (adress, secret) => {
   const email = {
-    from: "nico@prismagram.com",
+    from: "instagram@prismagram.com",
     to: adress,
     subject: "🔒Login Secret for Prismagram🔒",
     html: `Hello! Your login secret is <strong>${secret}</strong><br/>Copy paste on the app/website to log in`

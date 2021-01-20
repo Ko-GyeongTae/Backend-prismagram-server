a9b8939056ea52fa0ebe9b31419cb03b01b161b9
Ko-GyeongTae <kokt0203@naver.com>
2021-01-21 00:25:23 +0900

    Edit: seeFeed.js

--- src/api/Post/seeFeed/seeFeed.js
+++ src/api/Post/seeFeed/seeFeed.js
@@ -1,24 +1,19 @@
 import { prisma } from "../../../../generated/prisma-client";
 
 export default {
-    Query: {
-        seeFeed: async(_, __, {request, isAuthenticated}) => {
-            console.log("인증필요");
-            console.log(request.user);
-            isAuthenticated(request);
-            const { user } = request;
-            console.log(user);
-            const following = await prisma
-                .user({ id: user.id })
-                .following();
-            return prisma.posts({
-                where:{
-                    user:{
-                        id_in: [...following.map(user => user.id), user.id]
-                    }
-                },
-                orderBy: "createdAt_DESC"
-            });
-        }
+  Query: {
+    seeFeed: async (_, __, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { user } = request;
+      const following = await prisma.user({ id: user.id }).following();
+      return prisma.posts({
+        where: {
+          user: {
+            id_in: [...following.map(user => user.id), user.id]
+          }
+        },
+        orderBy: "createdAt_DESC"
+      });
     }
+  }
 };
